ec2dfacf359535162fe23c46b28649a4632daac7
Nikolay Nikolaev <n.nikolaevbg97@gmail.com>
2019-02-03 18:41:34 +0200

    Added Message System.

--- handlers/home.js
+++ handlers/home.js
@@ -6,12 +6,24 @@ module.exports.index = (req, res) => {
    let queryData = req.query;
 
    Product.find().populate('category').then((products) => {
-       if (queryData.query) {
-           products = products.filter(
+        if (queryData.query) {
+            products = products.filter(
                 p => p.name.toLowerCase()
                     .includes(queryData.query));
-       }
+        }
 
-       res.render('home/index', {products: products});
-   })
+        let data = { products };
+
+        if (req.query.error) {
+            data.error = req.query.error;
+        } else if (req.query.success) {
+            data.success = req.query.success;
+        }
+
+        res.render('home/index', {
+            products: products,
+            error: data.error,
+            success: data.success
+        });
+    })
 }
